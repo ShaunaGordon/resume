82d7203cadf2f78cb893afde5bb2e46efb8c68ee
Shauna Gordon <shauna@shaunagordon.com>
2018-06-28 18:24:34 -0400

    Update stuff. Update config accordingly.

--- vue.config.js
+++ vue.config.js
@@ -7,12 +7,6 @@ module.exports = {
     // enable CSS source maps?
     sourceMap: false,
 
-    // pass custom options to pre-processor loaders. e.g. to pass options to
-    // sass-loader, use { sass: { ... } }
-    loaderOptions: {
-      less: {'vue-style-loader!css-loader!less-loader'}
-    },
-
     // Enable CSS modules for all css / pre-processor files.
     // This option does not affect *.vue files.
     modules: false
