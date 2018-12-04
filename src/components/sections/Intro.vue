258fbb187108af69cc38e7c5561fd88f78e2c184
Shauna Gordon <shauna@shaunagordon.com>
2018-12-04 09:45:27 -0500

    Update Intro.vue

--- src/components/sections/Intro.vue
+++ src/components/sections/Intro.vue
@@ -3,10 +3,10 @@
         <header>
             <h2>About</h2>
             <p>
-                I'm a problem-solver by nature and by profession. I have a natural drive to find solutions to problems spoken and unspoken. By digging down through pain points, I excel in finding the heart of issues and coming up with the best solution.
+                I'm a problem-solver by nature and by profession. I have a natural drive to find solutions to problems spoken and unspoken. By digging down through pain points, I excel in finding the heart of issues and coming up with the best solution within the constraints of the environment.
             </p>
             <p>
-                While I currently specialize in PHP, I always enjoy learning new things, and currently have my sights set particularly on Go and Elixir programming, and diving deeper into Linux system administration and container management.
+                While I've historically specialized in PHP, I always enjoy learning new things, and am currently deep-diving into the world of Python, and have my sights set on Go and Elixir in the near future, as well as diving deeper into Linux system administration and container management.
             </p>
         </header>
     </section>
