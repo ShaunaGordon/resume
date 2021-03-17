c5f8ead3cb158cb811f45af993f4a7271f71481e
Shauna Gordon <shauna@shaunagordon.com>
2021-03-17 17:51:44 +0000

    Deploy ShaunaGordon/resume to github.com/ShaunaGordon/resume.git:gh-pages

---
+++ precache-manifest.6a972e83fa3057ae0d8b0fe29c26013e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7dc8769c9bbadaac938",
+    "url": "/css/app.8565247f.css"
+  },
+  {
+    "revision": "e000fbde6299c73bfd2b9d8f481f5497",
+    "url": "/img/avatar.png"
+  },
+  {
+    "revision": "3a93c445372d0379a31e474c0a55db56",
+    "url": "/img/bitmoji.png"
+  },
+  {
+    "revision": "3cc2264e2e5114200a127d2778ef87a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7dc8769c9bbadaac938",
+    "url": "/js/app.269590ce.js"
+  },
+  {
+    "revision": "6c1a1dfb696a9738fa49",
+    "url": "/js/chunk-vendors.05cd95f5.js"
+  },
+  {
+    "revision": "61369a716f298ee5be0a4260ec324cb0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
