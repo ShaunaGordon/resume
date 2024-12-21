b56bc99ff23e6215512ecf298a99ea2e4d45834c
Shauna Gordon <shauna@shaunagordon.com>
2024-12-20 20:09:56 -0500

    Tweaks
    Signed-off-by: Shauna Gordon <shauna@shaunagordon.com>

--- src/mixins/icons.js
+++ src/mixins/icons.js
@@ -1,10 +1,15 @@
+/* For reasons I don't really understand, Devicon uses "original" instead of "plain" for a handful of icons. */
+const classOriginal = [
+    'electron'
+];
+
 export const useIcons = () => {
     const getTechClass = (i) => {
         // Devicons doesn't have O3DE, so we're adding our own for now
         if(i == 'o3de') {
             return `icon devicon-cplusplus-plain`;
         }
-        return `icon devicon-${i}-plain`;
+        return `icon devicon-${i}-${classOriginal.includes(i) ? 'original' : 'plain'}`;
     };
 
     const getFaClass = (i) => {
